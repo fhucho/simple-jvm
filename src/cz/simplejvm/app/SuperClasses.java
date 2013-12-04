a55ff2ccfe4780705210fdf3b55d212008cf7d79
Martin <Martin@Martin>
2013-12-04 10:50:10 +0100

    .

---
+++ src/cz/simplejvm/app/SuperClasses.java
@@ -0,0 +1,41 @@
+
+package cz.simplejvm.app;
+
+public class SuperClasses {
+
+
+	public static void start() {
+		new SuperClasses().test();
+	}
+
+	void test() {
+		new Main().test();
+	}
+
+	private static class SuperMain {
+
+		NativeMethods nm = new NativeMethods();
+
+		public SuperMain() {
+			nm.print(11);
+		}
+		public void test() {
+			nm.print(1);
+		}
+
+	}
+
+	private static class Main extends SuperMain {
+
+		public Main() {
+			super();
+			nm.print(22);
+		}
+
+		@Override
+		public void test() {
+			nm.print(2);
+		}
+	}
+
+}
